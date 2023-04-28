edaa0b449d60e5d21d420afa318e7c645693124d
Mansi Ayer <42694815+MansiAyer@users.noreply.github.com>
2023-04-28 08:48:04 +0530

    testig documnt fixs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const originalRenderPage = ctx.renderPage
-
-    // Run the React rendering logic synchronously
-    ctx.renderPage = () =>
-      originalRenderPage({
-        // Useful for wrapping the whole react tree
-        enhanceApp: (App) => App,
-        // Useful for wrapping in a per-page basis
-        enhanceComponent: (Component) => Component,
-      })
+  static async getInitialProps(ctx) {
 
     // Run the parent `getInitialProps`, it now includes the custom `renderPage`
     const initialProps = await Document.getInitialProps(ctx)
