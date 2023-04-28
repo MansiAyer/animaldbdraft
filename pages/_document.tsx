4a560a80675fbfd304a80685f7009f0f4d87951e
Mansi Ayer <42694815+MansiAyer@users.noreply.github.com>
2023-04-28 08:48:59 +0530

    testig documnt fixs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: any) {
 
     // Run the parent `getInitialProps`, it now includes the custom `renderPage`
     const initialProps = await Document.getInitialProps(ctx)
